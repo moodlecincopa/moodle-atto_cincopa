b90cf1fffc1fc85a8cf59d8a84e4bc4461799b96
Bojan Mitrovic <Bojan Mitrovic>
2017-02-13 10:48:58 +0100

    Fixed console errors and all the same gallery.

--- yui/build/moodle-atto_cincopa-button/moodle-atto_cincopa-button-min.js
+++ yui/build/moodle-atto_cincopa-button/moodle-atto_cincopa-button-min.js
@@ -14,7 +14,9 @@ YUI.add('moodle-atto_cincopa-button', function (Y, NAME) {
                 callback: this._changeStyle
             });
             var secondbutton = this.addToolbarMenu({
-                items: items
+                items: items,
+                icon:'icontoolbar',
+                iconComponent: 'atto_cincopa'
             });
             secondbutton._node.removeChild(secondbutton._node.childNodes[0]);
             var po = this;
@@ -38,7 +40,7 @@ YUI.add('moodle-atto_cincopa-button', function (Y, NAME) {
                                         a.setAttribute('href', 'https://www.cincopa.com/login.aspx');
                                         a.setAttribute('role', 'link');
                                         a.setAttribute('target', '_blank');
-                                        li.setAttribute('class', 'atto_menuentry');
+                                        li.setAttribute('class', 'atto_menuentry2');
                                         a.innerHTML = "Login";
                                         li.appendChild(a);
                                         var li2 = document.createElement('li');
@@ -46,16 +48,14 @@ YUI.add('moodle-atto_cincopa-button', function (Y, NAME) {
                                         a2.setAttribute('href', 'https://www.cincopa.com/register.aspx');
                                         a2.setAttribute('role', 'link');
                                         a2.setAttribute('target', '_blank');
-                                        li2.setAttribute('class', 'atto_menuentry');
+                                        li2.setAttribute('class', 'atto_menuentry2');
                                         a2.innerHTML = "Register";
                                         li2.appendChild(a2);
                                         document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li);
                                         document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li2);
                                         document.getElementById('cincopa_atto').innerHTML = "";
                                     } else {
-                                        if (id.response.items_data.folders_count == 0) {
-
-                                        } else {
+                                        if (id.response.items_data.folders_count != 0) {                                     
                                             for (var i = 0; i < id.response.folders.length; i++) {
                                                 var did = id.response.folders[i].sysdata.did;
                                                 var li = document.createElement('li');
@@ -63,9 +63,10 @@ YUI.add('moodle-atto_cincopa-button', function (Y, NAME) {
                                                 a.setAttribute('href', '#');
                                                 a.setAttribute('role', 'menuitem');
                                                 a.setAttribute('data-index', 0);
-                                                a.addEventListener('click', function() {
+                                                a.setAttribute('did', id.response.folders[i].sysdata.did);
+                                                a.addEventListener('click', function(e) {
                                                     po.editor.focus();
-                                                    po.get('host').insertContentAtFocusPoint('[cincopa '+did+']');
+                                                    po.get('host').insertContentAtFocusPoint('[cincopa '+e.target.getAttribute('did')+']');
                                                     po.markUpdated();
                                                 });
                                                 li.setAttribute('role', 'presentation');
@@ -76,16 +77,22 @@ YUI.add('moodle-atto_cincopa-button', function (Y, NAME) {
                                             }
                                             document.getElementById('cincopa_atto').innerHTML = "";
                                         }
+                                        else{
+                                             document.getElementById('cincopa_atto').innerHTML = "No galleries found";
+
+                                        }
                                     }
                                 }
                             }
                         });
                     });
                 }, 500);
+                if(document.getElementById('cincopa_atto')){
                 while (document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.childNodes.length > 1) {
                     document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.removeChild(document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.lastChild);
                 }
                 document.getElementById('cincopa_atto').innerHTML = "wait...";
+            }
             });
         },
         _getUrl: function () {
--- yui/src/button/js/button.js
+++ yui/src/button/js/button.js
@@ -45,79 +45,86 @@ Y.namespace('M.atto_cincopa').Button = Y.Base.create('button', Y.M.editor_atto.E
             callback: this._changeStyle
         });
         var secondbutton = this.addToolbarMenu({
-            items: items
+            items: items,
+            icon:'icontoolbar',
+            iconComponent: 'atto_cincopa'
         });
         secondbutton._node.removeChild(secondbutton._node.childNodes[0]);
         var po = this;
-        secondbutton.on('click', function (e) {
-            window.setTimeout(function () {
-                YUI().use('jsonp', 'node', 'event', 'jsonp-url', function (Y) {
-                    Y.jsonp('https://www.cincopa.com/media-platform/my-galleries-getlist?disable_editor=true?callback=?', {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        sync: false,
-                        on: {
-                            success: function (id, response) {
-                                while (document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.childNodes.length > 1) {
-                                    document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.removeChild(document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.lastChild);
-                                }
-                                if (id.response.error) {
-                                    var li = document.createElement('li');
-                                    var a = document.createElement('a');
-                                    a.setAttribute('href', 'https://www.cincopa.com/login.aspx');
-                                    a.setAttribute('role', 'link');
-                                    a.setAttribute('target', '_blank');
-                                    li.setAttribute('class', 'atto_menuentry');
-                                    a.innerHTML = "Login";
-                                    li.appendChild(a);
-                                    var li2 = document.createElement('li');
-                                    var a2 = document.createElement('a');
-                                    a2.setAttribute('href', 'https://www.cincopa.com/register.aspx');
-                                    a2.setAttribute('role', 'link');
-                                    a2.setAttribute('target', '_blank');
-                                    li2.setAttribute('class', 'atto_menuentry');
-                                    a2.innerHTML = "Register";
-                                    li2.appendChild(a2);
-                                    document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li);
-                                    document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li2);
-                                    document.getElementById('cincopa_atto').innerHTML = "";
-                                } else {
-                                    if (id.response.items_data.folders_count == 0) {
-
-                                    } else {
-                                        for (var i = 0; i < id.response.folders.length; i++) {
-                                            var did = id.response.folders[i].sysdata.did;
-                                            var li = document.createElement('li');
-                                            var a = document.createElement('a');
-                                            a.setAttribute('href', '#');
-                                            a.setAttribute('role', 'menuitem');
-                                            a.setAttribute('data-index', 0);
-                                            a.addEventListener('click', function () {
-                                                po.editor.focus();
-                                                po.get('host').insertContentAtFocusPoint('[cincopa ' + did + ']');
-                                                po.markUpdated();
-                                            });
-                                            li.setAttribute('role', 'presentation');
-                                            li.setAttribute('class', 'atto_menuentry');
-                                            a.innerHTML = id.response.folders[i].sysdata.name + '(id:' + id.response.folders[i].sysdata.did + ')<br> Modified:' + id.response.folders[i].sysdata.modified;
-                                            li.appendChild(a);
-                                            document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li);
-                                        }
+            secondbutton.on('click', function (e) {
+                window.setTimeout(function () {
+                    YUI().use('jsonp', 'node', 'event', 'jsonp-url', function (Y) {
+                        Y.jsonp('https://www.cincopa.com/media-platform/my-galleries-getlist?disable_editor=true?callback=?', {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            sync: false,
+                            on: {
+                                success: function (id, response) {
+                                    while (document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.childNodes.length > 1) {
+                                        document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.removeChild(document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.lastChild);
+                                    }
+                                    if (id.response.error) {
+                                        var li = document.createElement('li');
+                                        var a = document.createElement('a');
+                                        a.setAttribute('href', 'https://www.cincopa.com/login.aspx');
+                                        a.setAttribute('role', 'link');
+                                        a.setAttribute('target', '_blank');
+                                        li.setAttribute('class', 'atto_menuentry2');
+                                        a.innerHTML = "Login";
+                                        li.appendChild(a);
+                                        var li2 = document.createElement('li');
+                                        var a2 = document.createElement('a');
+                                        a2.setAttribute('href', 'https://www.cincopa.com/register.aspx');
+                                        a2.setAttribute('role', 'link');
+                                        a2.setAttribute('target', '_blank');
+                                        li2.setAttribute('class', 'atto_menuentry2');
+                                        a2.innerHTML = "Register";
+                                        li2.appendChild(a2);
+                                        document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li);
+                                        document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li2);
                                         document.getElementById('cincopa_atto').innerHTML = "";
+                                    } else {
+                                        if (id.response.items_data.folders_count != 0) {                                     
+                                            for (var i = 0; i < id.response.folders.length; i++) {
+                                                var did = id.response.folders[i].sysdata.did;
+                                                var li = document.createElement('li');
+                                                var a = document.createElement('a');
+                                                a.setAttribute('href', '#');
+                                                a.setAttribute('role', 'menuitem');
+                                                a.setAttribute('data-index', 0);
+                                                a.setAttribute('did', id.response.folders[i].sysdata.did);
+                                                a.addEventListener('click', function(e) {
+                                                    po.editor.focus();
+                                                    po.get('host').insertContentAtFocusPoint('[cincopa '+e.target.getAttribute('did')+']');
+                                                    po.markUpdated();
+                                                });
+                                                li.setAttribute('role', 'presentation');
+                                                li.setAttribute('class', 'atto_menuentry');
+                                                a.innerHTML = id.response.folders[i].sysdata.name + '(id:' + id.response.folders[i].sysdata.did + ')<br> Modified:' + id.response.folders[i].sysdata.modified;
+                                                li.appendChild(a);
+                                                document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.appendChild(li);
+                                            }
+                                            document.getElementById('cincopa_atto').innerHTML = "";
+                                        }
+                                        else{
+                                             document.getElementById('cincopa_atto').innerHTML = "No galleries found";
+
+                                        }
                                     }
                                 }
                             }
-                        }
+                        });
                     });
-                });
-            }, 500);
-            while (document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.childNodes.length > 1) {
-                document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.removeChild(document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.lastChild);
+                }, 500);
+                if(document.getElementById('cincopa_atto')){
+                while (document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.childNodes.length > 1) {
+                    document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.removeChild(document.getElementById('cincopa_atto').parentNode.parentNode.parentNode.lastChild);
+                }
+                document.getElementById('cincopa_atto').innerHTML = "wait...";
             }
-            document.getElementById('cincopa_atto').innerHTML = "wait...";
-        });
+            });
     },
     _getUrl: function () {
         var width = screen.width - 220;
